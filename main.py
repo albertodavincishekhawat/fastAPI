0778754de3715e92e0f58bcba17f440e4c32f4a6
“[albertodavincishekhawat]” <[albertodavincishekhawat@gmail.com]>
2024-05-07 00:42:14 +0530

    POSTMAN

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ app = FastAPI()
 async def root():              #function
     return {"message": "welcome to api"}
 
+
+#path operations/ROUTE
+@app.get("/posts")                  # @decorator.http_method("/url")
+async def get_post():              #function
+    return {"data": "This is your post."}
+
