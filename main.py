49e493ad208f752b39f2e8a1fa994dd3e09a5003
“[albertodavincishekhawat]” <[albertodavincishekhawat@gmail.com]>
2024-05-08 09:08:52 +0530

    09.05.2024

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-#importing fastAPI
+#imports
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+
+
 
 #instance of fastapi
 app = FastAPI()
 
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+
+
+
 #path operations/ROUTE
-@app.get("/")                  # @decorator.http_method("/url")
-async def root():              #function
+@app.get("/")            # @decorator.http_method("/url")
+def root():              #function
     return {"message": "welcome to api"}
 
 
-#path operations/ROUTE
-@app.get("/posts")                  # @decorator.http_method("/url")
-async def get_post():              #function
+@app.get("/posts")              
+def get_post(s):
     return {"data": "This is your post."}
 
+
+@app.post("/createposts")              
+def create_posts(new_post:Post):  # type: ignore
+    print("new_post")
+    return {"data":"new post"}
+
+#title str, content str, category,Bool publish
